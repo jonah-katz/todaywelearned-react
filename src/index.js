184970c6631beefe58ba6fca47cd6c3301ee4ba8
jonah katz <jak1192@gmail.com>
2017-03-19 16:33:02 -0400

    Added Fastclick.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import 'sanitize.css/sanitize.css';
+import initReactFastclick from 'react-fastclick';
 
 import configureStore from './store';
 import Routes from './routes';
 
 const store = configureStore();
 
+initReactFastclick();
 
 render(
   <Provider store={store}>
